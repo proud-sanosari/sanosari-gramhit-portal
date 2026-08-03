a16183befefb7ca16fdd5b02352e4a831dbfde92
proud-sanosari <khodbhayajeegar@gmail.com>
2026-08-04 03:46:53 +0530

    Fix hero slider

--- js/app.js
+++ js/app.js
@@ -25,9 +25,9 @@ const slides = document.querySelectorAll(".slides img");
 let currentSlide = 0;
 
 if (slides.length > 0) {
+slides[0].classList.add("active");
   setInterval(() => {
       slides[currentSlide].classList.remove("active");
           currentSlide = (currentSlide + 1) % slides.length;
               slides[currentSlide].classList.add("active");
                 }, 5000);
-                }
